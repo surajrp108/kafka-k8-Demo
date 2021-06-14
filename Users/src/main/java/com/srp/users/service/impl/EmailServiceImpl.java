9fd9c4fa23ee9694a5968a5158a61a5920dddfe5
Suraj Prajapati <surajrp108@gmail.com>
2021-06-14 16:55:07 +0530

    1. Removed Kafka dependency 2. Removed Test 3. Added docker compose

--- Users/src/main/java/com/srp/users/service/impl/EmailServiceImpl.java
+++ Users/src/main/java/com/srp/users/service/impl/EmailServiceImpl.java
@@ -1,13 +1,11 @@
 package com.srp.users.service.impl;
 
 import com.srp.users.service.NotificationService;
-import org.eclipse.microprofile.reactive.messaging.Incoming;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Named;
-import javax.ws.rs.ApplicationPath;
 import java.util.Map;
 
 @ApplicationScoped
@@ -16,7 +14,6 @@ public class EmailServiceImpl implements NotificationService {
     private Logger log = LoggerFactory.getLogger(EmailServiceImpl.class);
 
     @Override
-    @Incoming("notification")
     public void send(String message) {
         log.info("Received notification for email with text: " + message);
     }
